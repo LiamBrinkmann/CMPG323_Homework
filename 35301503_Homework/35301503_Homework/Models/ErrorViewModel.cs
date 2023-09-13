5b3ecf34b70fe92a1e6cbb2b0b2a43c9c14acd74
LiamBrinkmann <112165336+LiamBrinkmann@users.noreply.github.com>
2023-09-13 22:33:05 +0200

    Kill it with fire
    It broke big time

--- 35301503_Homework/35301503_Homework/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _35301503_Homework.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
