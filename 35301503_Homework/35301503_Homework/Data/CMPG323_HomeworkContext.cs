c170c13b737c060f24897917cb546d214e17c517
LiamBrinkmann <112165336+LiamBrinkmann@users.noreply.github.com>
2023-09-13 17:37:06 +0200

    Followed the basics for the homework
    Added the basic implementation for homework. I do not understand what i did

---
+++ 35301503_Homework/35301503_Homework/Data/CMPG323_HomeworkContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using _35301503_Homework.Models;
+
+namespace _35301503_Homework.Data
+{
+    public partial class CMPG323_HomeworkContext : DbContext
+    {
+        public CMPG323_HomeworkContext()
+        {
+        }
+
+        public CMPG323_HomeworkContext(DbContextOptions<CMPG323_HomeworkContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; } = null!;
+        public virtual DbSet<Service> Services { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=CMPG323_Homework;TrustServerCertificate=true;Data Source=LIAM-PC\\SQLEXPRESS");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.ProductId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ProductID");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ProductDescription).IsUnicode(false);
+
+                entity.Property(e => e.ProductName).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Service>(entity =>
+            {
+                entity.ToTable("Service");
+
+                entity.Property(e => e.ServiceId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ServiceID");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ServiceDescription).IsUnicode(false);
+
+                entity.Property(e => e.ServiceName).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ 35301503_Homework/35301503_Homework/Repository/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using _35301503_Homework.Data;
+using _35301503_Homework.Models;
+using System.Collections;
+using System.Linq;
+
+namespace _35301503_Homework.Repository
+{
+    public class ProductRepository
+    {
+        protected readonly CMPG323_HomeworkContext _context = new CMPG323_HomeworkContext();
+
+        //GET ALL: Products
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Product.ToList();
+        }
+    }
+}
